0f63e76a311e78b535108565d3716a6532c62da9
Sönke Hahn <soenkehahn@gmail.com>
2018-03-03 18:42:56 -0500

    make install script work on mac

--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
 
-set -o errexit
+set -eux
 
 mkdir -p dist/bin
-go build -o dist/bin/jsi src/jsi.go
-upx dist/bin/jsi 1>&2
-echo compiled jsi 1>&2
+
+function compile () {
+  TARGET=dist/bin/jsi-$GOOS-$GOARCH
+  go build -o $TARGET src/jsi.go
+  if [ $GOOS == "linux" ]; then
+    upx $TARGET 1>&2
+  fi
+  echo compiled $TARGET 1>&2
+}
+
+export GOOS=linux
+export GOARCH=amd64
+compile
+
+export GOOS=darwin
+export GOARCH=amd64
+compile
+
+if [ -z "${INSTALL_GOOS:-}" ]; then
+  INSTALL_GOOS=$(uname | awk '{print tolower($0)}')
+fi
+INSTALL_GOARCH=amd64
+cp dist/bin/jsi-$INSTALL_GOOS-$INSTALL_GOARCH dist/bin/jsi
